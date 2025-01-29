0515b155f1a7f9193bddc24adfe325652cce9702
Sujin Kim <su10jin11@khu.ac.kr>
2025-01-30 00:15:21 +0900

    chore : 자바 컨벤션 적용

--- src/main/java/depromeet/onepiece/OnepieceApplication.java
+++ src/main/java/depromeet/onepiece/OnepieceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnepieceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnepieceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(OnepieceApplication.class, args);
+  }
 }
