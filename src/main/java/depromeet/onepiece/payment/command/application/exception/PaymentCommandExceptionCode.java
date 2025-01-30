3ba60b4d078d02c16c435b7c0b1d359faeee8759
Sujin Kim <108571492+cowboysj@users.noreply.github.com>
2025-01-30 15:18:20 +0900

    chore: Spotless 코드 포맷팅 + pre-commit 스크립트 작성 (#4)
    chore: Spotless 코드 포맷팅 + pre-commit 스크립트 작성 (#4)
    
    * chore : spotless 설정
    
    * chore : spotless 컨벤션 적용
    
    * chore : spotless pre-commit 적용
    
    * chore : spotless 자바 포맷 변경
    
    * chore : 자바 컨벤션 적용
    
    [Chore] Spotless 코드 포맷팅 + pre-commit 스크립트 작성 (#4)
    
    * chore : spotless 설정
    
    * chore : spotless 컨벤션 적용
    
    * chore : spotless pre-commit 적용
    
    * chore : spotless 자바 포맷 변경
    
    * chore : 자바 컨벤션 적용

--- src/main/java/depromeet/onepiece/payment/command/application/exception/PaymentCommandExceptionCode.java
+++ src/main/java/depromeet/onepiece/payment/command/application/exception/PaymentCommandExceptionCode.java
@@ -1,7 +1,4 @@
 package depromeet.onepiece.payment.command.application.exception;
 
-/**
- * CommandExceptionCode는 CommandService에서 발생하는 커스텀 예외 코드를 정의해요.
- */
-public enum PaymentCommandExceptionCode {
-}
+/** CommandExceptionCode는 CommandService에서 발생하는 커스텀 예외 코드를 정의해요. */
+public enum PaymentCommandExceptionCode {}
--- src/main/java/depromeet/onepiece/payment/query/application/PaymentQueryService.java
+++ src/main/java/depromeet/onepiece/payment/query/application/PaymentQueryService.java
@@ -1,8 +1,7 @@
 package depromeet.onepiece.payment.query.application;
 
 /**
- * 도메인 주도 개발 아키텍처에서 application 계층은 도메인 규칙에 핵심 로직 수행을 위임해요.
- * 따라서 QueryService는 조회한 데이터를 DTO로 변환하거나, 여러 도메인 객체를 조합하여 응답 데이터를 만드는 역할을 합니다.
+ * 도메인 주도 개발 아키텍처에서 application 계층은 도메인 규칙에 핵심 로직 수행을 위임해요. 따라서 QueryService는 조회한 데이터를 DTO로 변환하거나,
+ * 여러 도메인 객체를 조합하여 응답 데이터를 만드는 역할을 합니다.
  */
-public class PaymentQueryService {
-}
+public class PaymentQueryService {}
