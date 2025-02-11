7083508c03a7b62ebf3e130de3601b6c22b7bab9
Tae gyu Choi <suker800@gmail.com>
2025-02-11 21:26:48 +0900

    feat : PR 리뷰 수정

--- src/main/java/depromeet/onepiece/common/error/GlobalControllerAdvice.java
+++ src/main/java/depromeet/onepiece/common/error/GlobalControllerAdvice.java
@@ -3,7 +3,7 @@ package depromeet.onepiece.common.error;
 import java.nio.file.AccessDeniedException;
 import java.util.Objects;
 import java.util.stream.Collectors;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -13,87 +13,90 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestControllerAdvice
 @Slf4j
 public class GlobalControllerAdvice {
 
-	/**
-	 * Valid 검증 실패시 오류 발생
-	 * 주로 @RequestBody, @RequestPart 어노테이션에서 발생
-	 */
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<CustomResponse<Void>> handleValidationException(MethodArgumentNotValidException ex) {
-		String errorMessage = ex.getBindingResult().getFieldErrors()
-			.stream()
-			.map(error -> error.getField() + ": " + error.getDefaultMessage())
-			.collect(Collectors.joining(", "));
+  /** Valid 검증 실패시 오류 발생 주로 @RequestBody, @RequestPart 어노테이션에서 발생 */
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<CustomResponse<Void>> handleValidationException(
+      MethodArgumentNotValidException ex) {
+    String errorMessage =
+        ex.getBindingResult().getFieldErrors().stream()
+            .map(error -> error.getField() + ": " + error.getDefaultMessage())
+            .collect(Collectors.joining(", "));
 
-		log.error("Error Message : {}", errorMessage);
-		CustomResponse<Void> error = CustomResponse.error(GlobalErrorCode.VALID_EXCEPTION, errorMessage);
-		return ResponseEntity.status(error.getStatus()).body(error);
-	}
+    log.error("Error Message : {}", errorMessage);
+    CustomResponse<Void> error =
+        CustomResponse.error(GlobalErrorCode.VALID_EXCEPTION, errorMessage);
+    return ResponseEntity.status(error.getStatus()).body(error);
+  }
 
-	/**
-	 * 변수 Binding시 발생하는 오류
-	 **/
-	@ExceptionHandler(BindException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ResponseEntity<CustomResponse<Void>> invalidArgumentBindResponse(BindException e) {
-		log.error("Exception : {}, 입력값 : {}", e.getBindingResult().getFieldError(),
-			e.getBindingResult().getFieldError());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(CustomResponse.error(GlobalErrorCode.VALID_EXCEPTION,
-				Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()));
-	}
+  /** 변수 Binding시 발생하는 오류 */
+  @ExceptionHandler(BindException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<CustomResponse<Void>> invalidArgumentBindResponse(BindException e) {
+    log.error(
+        "Exception : {}, 입력값 : {}",
+        e.getBindingResult().getFieldError(),
+        e.getBindingResult().getFieldError());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(
+            CustomResponse.error(
+                GlobalErrorCode.VALID_EXCEPTION,
+                Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()));
+  }
 
-	/**
-	 * 지원하지 않은 HTTP method 호출 할 경우 발생
-	 */
-	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	protected ResponseEntity<CustomResponse<Void>> handleHttpRequestMethodNotSupportedException(
-		HttpRequestMethodNotSupportedException e) {
-		log.error("handleHttpRequestMethodNotSupportedException", e);
+  /** 지원하지 않은 HTTP method 호출 할 경우 발생 */
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+  protected ResponseEntity<CustomResponse<Void>> handleHttpRequestMethodNotSupportedException(
+      HttpRequestMethodNotSupportedException e) {
+    log.error("handleHttpRequestMethodNotSupportedException", e);
 
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(CustomResponse.error(GlobalErrorCode.METHOD_NOT_ALLOWED));
-	}
+    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+        .body(CustomResponse.error(GlobalErrorCode.METHOD_NOT_ALLOWED));
+  }
 
-	@ExceptionHandler(AccessDeniedException.class)
-	@ResponseStatus(HttpStatus.UNAUTHORIZED)
-	protected ResponseEntity<CustomResponse<Void>> handleAccessDeniedException(AccessDeniedException e) {
-		log.info("{}", e.getMessage());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(CustomResponse.error(GlobalErrorCode.ACCESS_DENIED));
-	}
+  @ExceptionHandler(AccessDeniedException.class)
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  protected ResponseEntity<CustomResponse<Void>> handleAccessDeniedException(
+      AccessDeniedException e) {
+    log.info("{}", e.getMessage());
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(CustomResponse.error(GlobalErrorCode.ACCESS_DENIED));
+  }
 
-	/**
-	 * 프로젝트내 설정한 예외가 발생할때 처리하는 부분
-	 *
-	 * @param e 발생한 예외
-	 * @return 예외를 처리해서 반환한다.
-	 */
-	@ExceptionHandler(GlobalException.class)
-	protected ResponseEntity<CustomResponse<Void>> handleGlobalBaseException(final GlobalException e) {
-		log.error("{} Exception {}: {}", e.getErrorCode(), e.getErrorCode().getCode(), e.getErrorCode().getMessage());
+  /**
+   * 프로젝트내 설정한 예외가 발생할때 처리하는 부분
+   *
+   * @param e 발생한 예외
+   * @return 예외를 처리해서 반환한다.
+   */
+  @ExceptionHandler(GlobalException.class)
+  protected ResponseEntity<CustomResponse<Void>> handleGlobalBaseException(
+      final GlobalException e) {
+    log.error(
+        "{} Exception {}: {}",
+        e.getErrorCode(),
+        e.getErrorCode().getCode(),
+        e.getErrorCode().getMessage());
 
-		return ResponseEntity.status(e.getErrorCode().getStatus())
-			.body(CustomResponse.error(e.getErrorCode(), e.getMessage()));
-	}
+    return ResponseEntity.status(e.getErrorCode().getStatus())
+        .body(CustomResponse.error(e.getErrorCode(), e.getMessage()));
+  }
 
-	/**
-	 * 처리되지 않은 에러를 여기서 처리 한다.
-	 *
-	 * @param e 발생한 에러
-	 * @return CustomResponse로 메시지를 감춰서 반환한다.
-	 */
-	@ExceptionHandler(Exception.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	protected ResponseEntity<CustomResponse<Void>> handleException(Exception e) {
-		log.error("Exception : {}", GlobalErrorCode.OTHER.getMessage(), e);
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-			.body(CustomResponse.error(GlobalErrorCode.OTHER));
-	}
+  /**
+   * 처리되지 않은 에러를 여기서 처리 한다.
+   *
+   * @param e 발생한 에러
+   * @return CustomResponse로 메시지를 감춰서 반환한다.
+   */
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  protected ResponseEntity<CustomResponse<Void>> handleException(Exception e) {
+    log.error("Exception : {}", GlobalErrorCode.OTHER.getMessage(), e);
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(CustomResponse.error(GlobalErrorCode.OTHER));
+  }
 }
--- src/main/java/depromeet/onepiece/common/error/GlobalErrorCode.java
+++ src/main/java/depromeet/onepiece/common/error/GlobalErrorCode.java
@@ -1,30 +1,24 @@
 package depromeet.onepiece.common.error;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import lombok.Getter;
-
 @Getter
+@AllArgsConstructor
 public enum GlobalErrorCode {
-	// 공통
-	SUCCESS(HttpStatus.OK, "G000", "요청에 성공했습니다."),
-	OTHER(HttpStatus.INTERNAL_SERVER_ERROR, "G100", "서버에 오류가 발생했습니다"),
+  // 공통
+  SUCCESS(HttpStatus.OK, "G000", "요청에 성공했습니다."),
+  OTHER(HttpStatus.INTERNAL_SERVER_ERROR, "G100", "서버에 오류가 발생했습니다"),
 
-	// 전체
-	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "G200", "허용되지 않은 메서드입니다"),
-	VALID_EXCEPTION(HttpStatus.BAD_REQUEST, "G300", "유효 하지 않은 요청입니다."),
-	ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "G400", "허용되지 않은 사용자입니다"),
-	TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "G500", "토큰이 만료되었습니다."),
-	USER_CONFLICT(HttpStatus.CONFLICT, "G600", "이미 가입된 내역이 있습니다."),
-
-	;
-	private final String code;
-	private final String message;
-	private final HttpStatus status;
-
-	GlobalErrorCode(final HttpStatus status, final String code, final String message) {
-		this.status = status;
-		this.code = code;
-		this.message = message;
-	}
+  // 전체
+  METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "GLOBAL_200", "허용되지 않은 메서드입니다"),
+  VALID_EXCEPTION(HttpStatus.BAD_REQUEST, "GLOBAL_300", "유효 하지 않은 요청입니다."),
+  ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "GLOBAL_400", "허용되지 않은 사용자입니다"),
+  TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "GLOBAL_500", "토큰이 만료되었습니다."),
+  USER_CONFLICT(HttpStatus.CONFLICT, "GLOBAL_600", "이미 가입된 내역이 있습니다."),
+  ;
+  private final HttpStatus status;
+  private final String code;
+  private final String message;
 }
