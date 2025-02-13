d6f4d3fc78fecb98538af978dbf0a81fa559d093
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2025-02-13 20:45:21 +0900

    feat : 글로벌 에러 핸들러 설정 (#13) (#14)
    * feat : 글로벌 에러 핸들러 설정 (#13)
    
    * feat : PR 리뷰 수정
    
    * feat : 컨벤션에 맞게 수정

---
+++ src/main/java/depromeet/onepiece/common/error/GlobalException.java
@@ -0,0 +1,13 @@
+package depromeet.onepiece.common.error;
+
+import lombok.Getter;
+
+/** 프로젝트 에러를 반환 할 때 사용하는 기본 클래스 errorCode를 주입받아야 한다. */
+public class GlobalException extends RuntimeException {
+  @Getter private final GlobalErrorCode errorCode;
+
+  public GlobalException(String message, GlobalErrorCode errorCode) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+}
