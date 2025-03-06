6a239aedf49722b354cd3d62c1cac8d50005f79f
Sujin Kim <108571492+cowboysj@users.noreply.github.com>
2025-03-06 21:24:13 +0900

    feat : pdf 파일 이미지 슬라이싱 구현 (#41)
    * feat : 이미지 슬라이싱 구현
    
    * feat : 이미지 슬라이싱 구현
    
    * feat : pdfBox 버전 업데이트
    
    * feat : pdfBox 버전 다운

--- src/main/java/depromeet/onepiece/file/command/infrastructure/ObjectStorageFileUploader.java
+++ src/main/java/depromeet/onepiece/file/command/infrastructure/ObjectStorageFileUploader.java
@@ -9,14 +9,20 @@ import depromeet.onepiece.file.command.exception.FileConvertErrorException;
 import depromeet.onepiece.file.command.exception.FileUploadFailedException;
 import depromeet.onepiece.file.domain.FileDocument;
 import depromeet.onepiece.file.domain.FileType;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import javax.imageio.ImageIO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -36,15 +42,7 @@ public class ObjectStorageFileUploader implements FileUploader {
   @Override
   public FileDocument upload(MultipartFile multipartFile, String logicalName, FileType fileType) {
     try {
-      File fileToUpload = convert(multipartFile).orElseThrow(FileConvertErrorException::new);
-      FileDocument fileDocumentToSave = FileDocument.create(new ObjectId(), logicalName, fileType);
-      String filePath =
-          generateUploadPath(fileDocumentToSave.getId().toString(), logicalName, fileType);
-      amazonS3.putObject(
-          new PutObjectRequest(bucketName, filePath, fileToUpload)
-              .withCannedAcl(CannedAccessControlList.Private));
-      removeUploadedFile(fileToUpload);
-      return fileRepository.save(fileDocumentToSave.setPhysicalPath(filePath));
+      return uploadPdf(multipartFile, logicalName, fileType);
     } catch (Exception e) {
       throw new FileUploadFailedException();
     }
@@ -73,12 +71,44 @@ public class ObjectStorageFileUploader implements FileUploader {
     return Optional.empty();
   }
 
-  private String generateUploadPath(String fileId, String logicalName, FileType fileType) {
-    String fileExtension = getFileExtension(logicalName);
-    String fileName = generateUniqueFilePath(fileExtension);
-    return switch (fileType) {
-      case PORTFOLIO_PDF -> Path.of(fileId, "upload", fileName).toString();
-      case PORTFOLIO_IMAGE -> Path.of(fileId, "processed", fileName).toString();
-    };
+  private FileDocument uploadPdf(
+      MultipartFile multipartFile, String logicalName, FileType fileType) {
+    try {
+      File pdfFile = convert(multipartFile).orElseThrow(FileConvertErrorException::new);
+      List<String> uploadedFilePaths = new ArrayList<>();
+      ObjectId fileId = new ObjectId();
+
+      String pdfUploadPath = Path.of(fileId.toString(), "upload", logicalName).toString();
+      amazonS3.putObject(
+          new PutObjectRequest(bucketName, pdfUploadPath, pdfFile)
+              .withCannedAcl(CannedAccessControlList.Private));
+      log.info("PDF uploaded to S3: " + pdfUploadPath);
+
+      try (PDDocument document = PDDocument.load(pdfFile)) {
+        PDFRenderer pdfRenderer = new PDFRenderer(document);
+        for (int page = 0; page < document.getNumberOfPages(); page++) {
+          BufferedImage image = pdfRenderer.renderImageWithDPI(page, 300);
+          String imageName = (page + 1) + ".png";
+          File imageFile = new File(imageName);
+          ImageIO.write(image, "png", imageFile);
+
+          String processedPath = Path.of(fileId.toString(), "processed", imageName).toString();
+          amazonS3.putObject(
+              new PutObjectRequest(bucketName, processedPath, imageFile)
+                  .withCannedAcl(CannedAccessControlList.Private));
+          log.info("Image uploaded to S3: " + processedPath);
+
+          uploadedFilePaths.add(processedPath);
+          removeUploadedFile(imageFile);
+        }
+      }
+
+      removeUploadedFile(pdfFile);
+      return fileRepository.save(
+          FileDocument.create(fileId, logicalName, fileType)
+              .setPhysicalPath(pdfUploadPath + "," + String.join(",", uploadedFilePaths)));
+    } catch (IOException e) {
+      throw new FileUploadFailedException();
+    }
   }
 }
