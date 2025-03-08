848f22374641be0969bfc6c174fe7a4d9e84c452
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-03-08 16:55:05 +0900

    refactor: 구글 OAuth 키워드 상수화

--- src/main/java/depromeet/onepiece/common/auth/application/dto/GoogleAuthAttributes.java
+++ src/main/java/depromeet/onepiece/common/auth/application/dto/GoogleAuthAttributes.java
@@ -7,15 +7,17 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GoogleAuthAttributes implements AuthAttributes {
+  private static final String EXTERNAL_ID_KEY = "sub";
+  private static final String EMAIL_KEY = "email";
 
   private final String id;
   private final String email;
   private final OAuthProviderType provide;
 
   public static GoogleAuthAttributes of(Map<String, Object> attributes) {
-    String id = (String) attributes.get("sub");
-    String email = (String) attributes.get("email");
-    return new GoogleAuthAttributes(id, email, OAuthProviderType.GOOGLE);
+    String externalId = (String) attributes.get(EXTERNAL_ID_KEY);
+    String email = (String) attributes.get(EMAIL_KEY);
+    return new GoogleAuthAttributes(externalId, email, OAuthProviderType.GOOGLE);
   }
 
   @Override
