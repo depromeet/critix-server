0e4dfa209b422950824afcf3e4a211e992a31a9a
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2025-03-08 23:51:18 +0900

    feat : 피드백 디테일 로직 구현 (#55)
    - 응답 Feedback으로 변경

--- src/main/java/depromeet/onepiece/feedback/command/presentation/FeedbackCommandController.java
+++ src/main/java/depromeet/onepiece/feedback/command/presentation/FeedbackCommandController.java
@@ -2,15 +2,21 @@ package depromeet.onepiece.feedback.command.presentation;
 
 import depromeet.onepiece.common.error.CustomResponse;
 import depromeet.onepiece.feedback.command.infrastructure.FeedbackService;
-import depromeet.onepiece.feedback.command.presentation.response.FeedbackResponse;
-import depromeet.onepiece.feedback.command.presentation.response.OverallFeedbackResponse;
-import depromeet.onepiece.feedback.command.presentation.response.ProjectFeedbackResponse;
 import depromeet.onepiece.feedback.command.presentation.response.RecentFeedbackListResponse;
 import depromeet.onepiece.feedback.command.presentation.response.RemainCountResponse;
+import depromeet.onepiece.feedback.domain.EvaluationDetail;
+import depromeet.onepiece.feedback.domain.Feedback;
+import depromeet.onepiece.feedback.domain.FeedbackContent;
+import depromeet.onepiece.feedback.domain.FeedbackDetail;
+import depromeet.onepiece.feedback.domain.FeedbackPerPage;
+import depromeet.onepiece.feedback.domain.FeedbackType;
+import depromeet.onepiece.feedback.domain.OverallEvaluation;
+import depromeet.onepiece.feedback.domain.ProjectEvaluation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
@@ -38,46 +44,51 @@ public class FeedbackCommandController {
 
   @Operation(summary = "포트폴리오 응답 가져오기", description = "포트폴리오 피드백을 feedback id로 가져오기")
   @GetMapping("")
-  public ResponseEntity<CustomResponse<FeedbackResponse>> getFeedbackDetail(
+  public ResponseEntity<CustomResponse<Feedback>> getFeedbackDetail(
       @Parameter(example = "66e516c2b355355088f07c82")
           @RequestParam(defaultValue = "66e516c2b355355088f07c82")
           ObjectId feedbackId) {
 
-    OverallFeedbackResponse overallFeedbackResponse =
-        new OverallFeedbackResponse(
-            new OverallFeedbackResponse.OverallEvaluation(
-                "종합 평가 요약 ",
-                new OverallFeedbackResponse.EvaluationDetail(80, "직무 적합성이 높습니다."),
-                new OverallFeedbackResponse.EvaluationDetail(75, "논리적인 사고력이 돋보입니다."),
-                new OverallFeedbackResponse.EvaluationDetail(85, "글쓰기 표현이 명확합니다."),
-                new OverallFeedbackResponse.EvaluationDetail(90, "레이아웃이 가독성이 높습니다.")),
+    List<FeedbackDetail> strengths =
+        List.of(new FeedbackDetail("데이터 기반의 디자인 사고", List.of("a/b 테스트 설문조사", "단순한 미적감각이 아니라 뛰어남")));
+    List<FeedbackDetail> improvements =
+        List.of(
+            new FeedbackDetail(
+                "UI/UX 디자인 과정을 시각적으로 추가하기", List.of("더 강조할 필요가 있음", "더 개선할 여지가 있음. ")));
+    OverallEvaluation overallEvaluation =
+        new OverallEvaluation(
+            "종합 평가 요약 ",
+            new EvaluationDetail(80, "직무 적합성이 높습니다."),
+            new EvaluationDetail(75, "논리적인 사고력이 돋보입니다."),
+            new EvaluationDetail(85, "글쓰기 표현이 명확합니다."),
+            new EvaluationDetail(90, "레이아웃이 가독성이 높습니다."),
+            strengths,
+            improvements);
+
+    ProjectEvaluation projectEvaluation =
+        new ProjectEvaluation(
+            "프로젝트 1",
+            List.of(true, false, false, false, false),
+            "회고가 빠져있습니다",
             List.of(
-                new OverallFeedbackResponse.FeedbackDetail(
-                    "데이터 기반의 디자인 사고", List.of("a/b 테스트 설문조사", "단순한 미적감각이 아니라 뛰어남"))),
+                new FeedbackDetail(
+                    "데이터 기반의 문제 해결", List.of("단순한 ui 개선이아닌 ", "새소식 사용자의 50%가 모른다 답변"))),
+            List.of(new FeedbackDetail("문장이 길고 가독성이 낮음", List.of("설명이 너무 상세하여 핵심을 빠르게 파악하기 어려움"))),
             List.of(
-                new OverallFeedbackResponse.FeedbackDetail(
-                    "UI/UX 디자인 과정을 시각적으로 추가하기", List.of("더 강조할 필요가 있음", "더 개선할 여지가 있음. "))));
-    FeedbackResponse feedbackResponse =
-        new FeedbackResponse(
-            overallFeedbackResponse,
-            new ProjectFeedbackResponse(
-                new ProjectFeedbackResponse.ProjectEvaluation(
-                    "프로젝트 1",
-                    "https://avatars.githubusercontent.com/u/108571492?v=4",
-                    List.of(true, false, false, false, false),
-                    "프로젝트의 핵심 프로세스 중 일부가 잘 반영되었으며, 개선이 필요한 부분도 존재합니다.",
-                    "A/B 테스트와 유저 피드백을 활용한 데이터 중심 디자인이 강점입니다.",
-                    "페이지 간 일관성이 부족하고, 일부 텍스트의 가독성이 떨어집니다.",
-                    List.of(
-                        new ProjectFeedbackResponse.PageIssue(
-                            "1",
-                            "https://avatars.githubusercontent.com/u/108571492?v=4",
-                            List.of(
-                                new ProjectFeedbackResponse.PageContentIssue(
-                                    "번역체/어색한 표현",
-                                    "A designer who wants to do many things.",
-                                    "다양한 디자인을 통해 다재다능함을 보여주는 디자이너입니다.")))))));
-    return CustomResponse.okResponseEntity(feedbackResponse);
+                new FeedbackPerPage(
+                    "1",
+                    List.of(new FeedbackContent(FeedbackType.LOGICAL_LEAP, "기존문장", "바뀐문장")),
+                    "https://avatars.githubusercontent.com/u/108571492?v=4")),
+            "전체적으로 훌륭한 프로젝트입니다.");
+    Feedback feedback =
+        new Feedback(
+            new ObjectId(),
+            new ObjectId(),
+            new ObjectId(),
+            overallEvaluation,
+            new ArrayList<>(),
+            projectEvaluation);
+    return CustomResponse.okResponseEntity(feedback);
   }
 
   @Operation(summary = "최근 포폴 피드백 목록", description = "최근 피드백 목록 기본 날짜정렬 ")
--- src/main/java/depromeet/onepiece/feedback/domain/ProjectEvaluation.java
+++ src/main/java/depromeet/onepiece/feedback/domain/ProjectEvaluation.java
@@ -1,14 +1,16 @@
 package depromeet.onepiece.feedback.domain;
 
-import static lombok.AccessLevel.PROTECTED;
+import static lombok.AccessLevel.*;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
 public class ProjectEvaluation {
 
   @Field("project_name")
@@ -21,10 +23,10 @@ public class ProjectEvaluation {
   private String processReview;
 
   @Field("positiveFeedback")
-  private String positiveFeedback;
+  private List<FeedbackDetail> positiveFeedback;
 
   @Field("negativeFeedback")
-  private String negativeFeedback;
+  private List<FeedbackDetail> negativeFeedback;
 
   @Field("feedback_per_page")
   private List<FeedbackPerPage> feedbackPerPage;
