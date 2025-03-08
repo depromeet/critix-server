f61fd8ea34ac6cc001aaa699332b3993743f38c0
Sujin Kim <su10jin11@khu.ac.kr>
2025-03-08 17:23:46 +0900

    feat : ncp에서 이미지링크 가져오기

---
+++ src/main/java/depromeet/onepiece/file/command/infrastructure/PresignedUrlGenerator.java
@@ -0,0 +1,64 @@
+package depromeet.onepiece.file.command.infrastructure;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PresignedUrlGenerator {
+
+  private final AmazonS3 amazonS3;
+
+  @Value("${cloud.ncp.object-storage.credentials.bucket}")
+  private String bucketName;
+
+  public List<String> generatePresignedUrl(String folderPath) {
+    List<S3ObjectSummary> objectSummaries =
+        amazonS3.listObjects(bucketName, folderPath + "/processed").getObjectSummaries();
+
+    objectSummaries.sort(Comparator.comparing(o -> extractSortableKey(o.getKey())));
+
+    List<String> presignedUrls = new ArrayList<>();
+    for (S3ObjectSummary objectSummary : objectSummaries) {
+      String objectKey = objectSummary.getKey();
+      URL presignedUrl = generatePresignedUrlForObject(objectKey, 30);
+      presignedUrls.add(presignedUrl.toString());
+    }
+
+    return presignedUrls;
+  }
+
+  private URL generatePresignedUrlForObject(String objectKey, int expirationMinutes) {
+    Date expiration = new Date();
+    expiration.setTime(System.currentTimeMillis() + expirationMinutes * 60 * 1000);
+
+    GeneratePresignedUrlRequest generatePresignedUrlRequest =
+        new GeneratePresignedUrlRequest(bucketName, objectKey)
+            .withMethod(HttpMethod.GET)
+            .withExpiration(expiration);
+
+    return amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
+  }
+
+  private String extractSortableKey(String filename) {
+    Pattern pattern = Pattern.compile("(\\d+)");
+    Matcher matcher = pattern.matcher(filename);
+    StringBuffer stringBuffer = new StringBuffer();
+
+    while (matcher.find()) {
+      String number = matcher.group(1);
+      matcher.appendReplacement(stringBuffer, String.format("%020d", Long.parseLong(number)));
+    }
+    matcher.appendTail(stringBuffer);
+    return stringBuffer.toString();
+  }
+}
