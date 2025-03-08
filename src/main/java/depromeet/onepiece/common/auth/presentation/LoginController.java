f57d529f6308837442ff6ba6ebece619b6852d4b
이한음 <leehaneum160924@kyonggi.ac.kr>
2025-03-08 21:27:43 +0900

    chore: nginx 설정 docker-compose.yml 경로 수정 (#52)

--- src/main/java/depromeet/onepiece/common/auth/presentation/LoginController.java
+++ src/main/java/depromeet/onepiece/common/auth/presentation/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Profile("local")
+@Profile({"local", "dev"})
 @Controller
 @RequestMapping("/login")
 public class LoginController {
