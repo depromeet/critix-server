827cbb80426892f298a1982a2914b0dc8b815ef9
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-03-10 23:41:03 +0900

    fix: dev.critix.kr CORS 방지

--- src/main/java/depromeet/onepiece/common/config/SecurityConfig.java
+++ src/main/java/depromeet/onepiece/common/config/SecurityConfig.java
@@ -100,7 +100,7 @@ public class SecurityConfig {
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowedHeaders(Collections.singletonList("*"));
       config.setAllowedMethods(Collections.singletonList("*"));
-      config.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+      config.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://dev.critix.kr"));
       config.setAllowCredentials(true);
       return config;
     };
