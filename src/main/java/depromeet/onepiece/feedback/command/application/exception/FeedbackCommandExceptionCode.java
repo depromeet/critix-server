b63b7ab99e3a95c8bef4526c4563519bbc5843de
Sujin Kim <108571492+cowboysj@users.noreply.github.com>
2025-03-30 16:20:25 +0900

    feat : OCR 결과 폴링 (#124)
    * feat : OCR 결과 폴링 구현
    
    * feat : 예외 추가

--- src/main/java/depromeet/onepiece/feedback/command/application/exception/FeedbackCommandExceptionCode.java
+++ src/main/java/depromeet/onepiece/feedback/command/application/exception/FeedbackCommandExceptionCode.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum FeedbackCommandExceptionCode implements ErrorCode {
   FEEDBACK_CHATGPT_ERROR(BAD_REQUEST, "FEEDBACK_100", "ChatGPT 요청에 실패하였습니다."),
+  OCR_NOT_FOUND(BAD_REQUEST, "FEEDBACK_101", "OCR 결과를 찾을 수 없습니다."),
+  GET_OCR_RESULT_FAILED(BAD_REQUEST, "FEEDBACK_102", "OCR 결과를 가져오는데 실패하였습니다."),
   ;
 
   private final HttpStatus status;
